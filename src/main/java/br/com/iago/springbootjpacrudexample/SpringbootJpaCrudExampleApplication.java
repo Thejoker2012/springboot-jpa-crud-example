e33d86c43aad97550135c15f2396b5b38451f40b
Iago Santos <iagofalcom@hotmail.com>
2019-08-22 19:52:46 -0300

    Primeiro commit

---
+++ src/main/java/br/com/iago/springbootjpacrudexample/SpringbootJpaCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.iago.springbootjpacrudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaCrudExampleApplication.class, args);
+	}
+
+}
